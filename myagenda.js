13f83f1fdec0a19fa7f8a01e563cca722b8c3b20
Begüm Filiz Öz <113853370+begumfilizoz@users.noreply.github.com>
2024-04-08 14:10:52 +0300

    Code update with weather element & new images

--- myagenda.js
+++ myagenda.js
@@ -1,5 +1,11 @@
 const inputBox = document.getElementById("input-box");
 const list = document.getElementById("list-container");
+const apiKey = "641b6c86de7f520632a3281c124732f1";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector(".row2 input");
+const searchButton = document.querySelector(".row2 button");
+const weatherImg = document.querySelector(".weather-icon");
+findWeather("Zurich");
 function addTask()
 {
     if(inputBox.value === '')
@@ -40,3 +46,46 @@ function bringTasks()
     list.innerHTML = localStorage.getItem("tasks");
 }
 bringTasks();
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+});
+document.getElementById('date').textContent = formattedDate;
+async function findWeather(city)
+{
+    const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&deg;C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if (data.weather[0].main == "Clouds")
+    {
+        weatherImg.src = "cloudy.png";
+    }
+    else if (data.weather[0].main == "Clear")
+    {
+        weatherImg.src = "sunny.png";
+    }
+    else if (data.weather[0].main == "Rain")
+    {
+        weatherImg.src = "rainy.png";
+    }
+    else if (data.weather[0].main == "Drizzle")
+    {
+        weatherImg.src = "drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist")
+    {
+        weatherImg.src = "fog.png";
+    }
+    
+
+}
+searchButton.addEventListener("click", ()=>{
+    findWeather(searchBox.value);
+});
